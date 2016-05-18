1cc72fcaf1cfc29ba4d7906b3e1b6e8202ac6d81
monster-hui <373000945@qq.com>
2016-05-18 11:25:39 +0800

    add  function
    添加函数体

--- src/fibonacci_series.js
+++ src/fibonacci_series.js
@@ -1,7 +1,14 @@
 'use strict';
 
 function fibonacci_series(n) {
-
+	var fib=[];
+	for(var i=0;i<n+1;i++){
+		if(i<2)fib[i]=i;
+		else{
+			fib[i]=fib[i-2]+fib[i-1];
+		}
+	}
+	return fib;
 }
 
 module.exports = fibonacci_series;
